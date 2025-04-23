b4dd0f37f5ed4c08d6898793707d37fb2c9e5d7a
Conlanmann9 <111758120+Conlanmann9@users.noreply.github.com>
2025-04-23 11:14:03 -0600

    Delete Personal-Website-main directory

--- Personal-Website-main/js/getheader.js
+++
@@ -1,5 +0,0 @@
-fetch('headerContainer.html')
-    .then(response => response.text())
-        .then(html => {
-            document.getElementById('header-placeholder').innerHTML += html;
-        });
